4aba720810e75f2209b1673fa80b59b9b072505b
saket1612 <saket.garg.1234@gmail.com>
2021-06-26 18:18:07 +0530

    first commit

---
+++ src/carparking/CarParking.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carparking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * @author Dell
+ */
+class Vehicle {
+
+    String registration_num;
+    int age;
+
+    Vehicle(String registration_num, int age) {
+        this.registration_num = registration_num;
+        this.age = age;
+    }
+}
+
+public class CarParking {
+
+    /**
+     * @param args the command line arguments
+     */
+    private final HashMap<Integer, Vehicle> slot_vehicle_mapping;
+    private final HashMap<String, Integer> registration_slot_mapping;
+    private final HashMap<Integer, List<Integer>> age_slot_mapping;
+    private final HashMap<Integer, List<String>> age_registration_mapping;
+    private final PriorityQueue<Integer> available_slots;
+
+    CarParking(int slots) {
+        slot_vehicle_mapping = new HashMap<>();
+        registration_slot_mapping = new HashMap<>();
+        age_slot_mapping = new HashMap<>();
+        age_registration_mapping = new HashMap<>();
+
+        available_slots = new PriorityQueue<>();
+        for (int i = 1; i <= slots; i++) {
+            available_slots.add(i);
+        }
+
+    }
+
+    //Returns nearest slot to entry point otherwise 0
+    private int getNearestSlot() {
+        if (available_slots.isEmpty()) {
+            return 0;
+        }
+
+        int slot = available_slots.peek();
+        available_slots.poll();
+        return slot;
+    }
+
+    public int park(Vehicle v) {
+        int slot = getNearestSlot();
+
+        //All slots are full
+        if (slot == 0) {
+            return 0;
+        }
+
+        slot_vehicle_mapping.put(slot, v);
+        registration_slot_mapping.put(v.registration_num, slot);
+
+        if (age_slot_mapping.get(v.age) == null) {
+            age_slot_mapping.put(v.age, new ArrayList<>());
+        }
+        age_slot_mapping.get(v.age).add(slot);
+
+        if (age_registration_mapping.get(v.age) == null) {
+            age_registration_mapping.put(v.age, new ArrayList<>());
+        }
+        age_registration_mapping.get(v.age).add(v.registration_num);
+
+        return slot;
+    }
+
+    public Vehicle leave(int slot) {
+        //If the slot is already empty
+        if (slot_vehicle_mapping.containsKey(slot) == false) {
+            return null;
+        }
+
+        available_slots.add(slot);
+        Vehicle v = slot_vehicle_mapping.get(slot);
+        slot_vehicle_mapping.remove(slot);
+        registration_slot_mapping.remove(v.registration_num);
+        age_slot_mapping.get(v.age).remove(new Integer(slot));
+        age_registration_mapping.get(v.age).remove(v.registration_num);
+        return v;
+    }
+
+    public List<String> getRegNumberForDriverAge(int age) {
+        //No Registration number are assigned to persons of this age
+        if (age_registration_mapping.get(age) == null) {
+            return new ArrayList<>();
+        }
+
+        return age_registration_mapping.get(age);
+    }
+
+    public List<Integer> getSlotsForDriverAge(int age) {
+        //No slots are assigned to persons of this age
+        if (age_slot_mapping.get(age) == null) {
+            return new ArrayList<>();
+        }
+
+        return age_slot_mapping.get(age);
+    }
+
+    public Integer getSlotForRegNumber(String reg_num) {
+        //No slot is assigned to given registration number
+        if (registration_slot_mapping.get(reg_num) == null) {
+            return 0;
+        }
+
+        return registration_slot_mapping.get(reg_num);
+    }
+
+}
---
+++ src/carparking/ProcessInput.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carparking;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ *
+ * @author Dell
+ */
+public class ProcessInput {
+
+    private CarParking parking_lot;
+
+    //Function to check if given String is Integer or not
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    //Function to process the given Input
+    public void process(String st) throws FileNotFoundException {
+        String[] arr = st.split(" ");
+        String command = arr[0];
+
+        switch (command) {
+            case "Create_parking_lot":
+                if (arr.length == 2 && isNumeric(arr[1])) {
+                    int slots = Integer.parseInt(arr[1]);
+                    //Creating a parking lot of given slots
+                    parking_lot = new CarParking(slots);
+                    
+                    //Storing the output to Output.txt
+                    PrintStream o = new PrintStream(new File("Output.txt"));
+                    System.setOut(o);
+                    System.out.println("Created parking of " + slots + " slots");
+                }
+                break;
+
+            case "Park":
+                if (arr.length == 4 && arr[2].equals("driver_age")) {
+                    Vehicle car = new Vehicle(arr[1], Integer.parseInt(arr[3]));
+                    int slot = parking_lot.park(car);
+
+                    //All slots are full so no parking for this car
+                    if (slot == 0) {
+                        System.out.println();
+                    } else {
+                        System.out.println("Car with vehicle registration number " + arr[1] + " has been parked at slot number " + slot);
+                    }
+                }
+                break;
+
+            case "Leave":
+                if (arr.length == 2 && isNumeric(arr[1])) {
+                    int slot = Integer.parseInt(arr[1]);
+                    Vehicle v = parking_lot.leave(slot);
+                    //If vehicle is not present at given slot
+                    if (v == null) {
+                        System.out.println();
+                    } else {
+                        System.out.println("Slot number " + slot + " vacated, the car with vehicle registration number "
+                                + v.registration_num + " left the space, the driver of the car was of age " + v.age);
+                    }
+                }
+                break;
+
+            case "Slot_numbers_for_driver_of_age":
+                if (arr.length == 2) {
+                    List<Integer> slots = parking_lot.getSlotsForDriverAge(Integer.parseInt(arr[1]));
+                    slots.forEach((i) -> {
+                        System.out.print(i + " ");
+                    });
+                    System.out.println();
+                }
+                break;
+
+            case "Slot_number_for_car_with_number":
+                if (arr.length == 2) {
+                    int slot = parking_lot.getSlotForRegNumber(arr[1]);
+                    //No slot is assigned to given registration number
+                    if (slot == 0) {
+                        System.out.println();
+                    } else {
+                        System.out.println(slot);
+                    }
+                }
+                break;
+
+            case "Vehicle_registration_number_for_driver_of_age":
+                if (arr.length == 2) {
+                    List<String> numbers = parking_lot.getRegNumberForDriverAge(Integer.parseInt(arr[1]));
+                    numbers.forEach((num) -> {
+                        System.out.print(num+" ");
+                    });
+                    System.out.println();
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Reading inputs from Input.txt
+        ProcessInput obj = new ProcessInput();
+        File file = new File("Input.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null) {
+            obj.process(st);
+        }
+    }
+}
