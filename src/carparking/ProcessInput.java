37b444edba7e454988233c3ac43374bfe443b7e9
saket1612 <saket.garg.1234@gmail.com>
2021-06-26 18:20:20 +0530

    Comments corrected

--- src/carparking/ProcessInput.java
+++ src/carparking/ProcessInput.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package carparking;
 
 import java.io.*;
 import java.util.List;
 
-/**
- *
- * @author Dell
- */
+
 public class ProcessInput {
 
     private CarParking parking_lot;
